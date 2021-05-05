0f73044afc4e9cdff5af63265f6a95d046362474
d-imosemi <83042447+d-imosemi@users.noreply.github.com>
2021-05-05 13:52:20 +0100

    Add files via upload

---
+++ quiz-game-start-1/main.py
@@ -0,0 +1,21 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import Quizbrain
+
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = Quizbrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("you have complited the quiz")
+print(f"your final score is {quiz.score}/{quiz.question_number}")
+
